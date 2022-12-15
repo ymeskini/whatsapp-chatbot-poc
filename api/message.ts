a35dc37087e3a6d1f0ca1cee31aa4b876044cece
Youssef Meskini <youss.meskini@gmail.com>
2022-12-15 08:51:14 +0100

    add verify token step

--- api/message.ts
+++ api/message.ts
@@ -1,8 +1,37 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  console.log(req.body);
-  res.json({
-    status: "ok",
-  });
+  switch (req.method) {
+    case "POST":
+      console.log(req.body);
+      res.json({ status: "ok" });
+      break;
+
+    case "GET":
+      // Parse params from the webhook verification request
+      const mode = req.query["hub.mode"];
+      const token = req.query["hub.verify_token"];
+      const challenge = req.query["hub.challenge"];
+
+      // Check if a token and mode were sent
+      if (mode && token) {
+        // Check the mode and token sent are correct
+        if (
+          mode === "subscribe" &&
+          token === process.env.WHATSAPP_VERIFY_TOKEN
+        ) {
+          // Respond with 200 OK and challenge token from the request
+          console.log("WEBHOOK_VERIFIED");
+          return res.status(200).send(challenge);
+        } else {
+          // Responds with '403 Forbidden' if verify tokens do not match
+          return res.status(403).send("Forbidden");
+        }
+      }
+    default:
+      res.json({
+        status: "ok",
+      });
+      break;
+  }
 }
